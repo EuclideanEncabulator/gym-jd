fe8b127beffd974ba2e133b9ec2f0d9fe95812db
Kamron Bhavnagri <kamwithk@tuta.io>
2021-05-02 17:21:29 +1000

    Action and observation improvements

--- gym_jd/envs/jd_env.py
+++ gym_jd/envs/jd_env.py
@@ -16,7 +16,6 @@ class JDEnv(Env):
     def __init__(self, jd_path):
         ONE_SHAPE = (1,)
 
-        # TODO: Check whether mix of continuous and discrete actions works
         self.action_space = Dict({
             "steering": Box(low=-1, high=1, shape=ONE_SHAPE),
             "braking": Discrete(2),
@@ -25,31 +24,37 @@ class JDEnv(Env):
         self.observation_space = Dict({
             "speed": Box(low=0, high=np.inf, shape=ONE_SHAPE),
             "direction": Box(low=-1, high=1, shape=(4,)), # quaternion
-            "position": Box(low=-np.inf, high=np.inf, shape=(2,)), # get coord bounds
+            "position": Box(low=-np.inf, high=np.inf, shape=(2,)),
             "grounded": Discrete(2),
-            "wheel_direction": Box(low=-1, high=1, shape=ONE_SHAPE), # get rotation bounds
-            "next_nodes": Box(low=-np.inf, high=np.inf, shape=(3, 3)) # get coord bounds
+            "wheel_direction": Box(low=-1, high=1, shape=ONE_SHAPE),
+            "next_nodes": Box(low=-np.inf, high=np.inf, shape=(3, 3))
         })
 
         self.nodes = np.load(pkg_resources.resource_filename("extra", "nodes.npy"))
 
         self.process = Process(jd_path)
-        sleep(5) # TODO: Move to c++, we can tell when unity has loaded.
-        inject(self.process.pid, pkg_resources.resource_filename("extra", "jelly_drift_interface.dll"))
+        sleep(5) # TODO: Move to c++, we can tell when unity has loaded
+        dll_path = pkg_resources.resource_filename("extra", "jelly_drift_interface.dll").replace("\\", "/")
+        inject(self.process.pid, dll_path.encode("ascii"))
 
     def reset(self):
-        # TODO: Start a new episode from the beginning
-        self.nodes = np.load(pkg_resources.resource_filename("extra", "nodes.npy"))
+        return self.action_observation(reset=True)
 
-        # return observation
-        pass
+    def action_observation(self, reset: bool=False, steering: float=0.0, throttle: float=1., braking: bool=False):
+        self.process.write({
+            "reset": reset,
+            "steering": steering,
+            "throttle": throttle,
+            "braking": braking
+        })
+
+        return self.process.read()
 
     def step(self, action):
         PSEUDO_MAX_SPEED = 300
         CONSIDER_NODES, PROXIMITY_RADIUS = 5, 5
 
-        # Get observation/state from IPC
-        observation = self.process.read()
+        observation = self.action_observation(*action)
 
         distances, index = cdist([observation["position"]], self.nodes[:CONSIDER_NODES]), np.arange(CONSIDER_NODES)
         path_proximity = np.reciprocal(distances * (index + 1) ** 2).sum()
@@ -59,15 +64,8 @@ class JDEnv(Env):
         self.nodes = np.delete(self.nodes, (distances < PROXIMITY_RADIUS).nonzero())
         done = self.nodes.size != 0
 
-        observation["speed"] = observation["speed"][0] # TODO: Check whether intended
+        observation["speed"] = observation["speed"][0]
         reward = (observation["speed"] / PSEUDO_MAX_SPEED) * path_proximity
-
-        self.process.write({
-            "reset": False,
-            "steering": 0.0,
-            "throttle": random.random(),
-            "braking": False
-        })
 
         info = {} # extra info for debugging
         return observation, reward, done, info
