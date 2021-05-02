ce9553037b53ecd36c31f42ad5f305c84cf3ee95
Kamron Bhavnagri <kamwithk@tuta.io>
2021-05-02 23:22:25 +1000

    Pip fixes (#1)
    * More pip provided information
    
    * Extra empty Python init files
    
    * Python manifest for pip package
    
    * Compiled dll

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(name="gym_jd",
       version="0.0.1",
       install_requires=["gym"],
-      include_package_data=True
+      include_package_data=True,
+      packages=find_packages()
 )
