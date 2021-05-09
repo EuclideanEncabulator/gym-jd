d70a661b4af071af6c23291b9e525fca139656ab
Kamron Bhavnagri <kamwithk@tuta.io>
2021-05-09 19:08:05 +1000

    Average velocity and dynamic max episode lenghts

--- gym_jd/envs/jd_env.py
+++ gym_jd/envs/jd_env.py
@@ -11,6 +11,7 @@ from multiprocessing import shared_memory
 from gym_jd.interface.python.injector import inject
 from gym_jd.interface.python.ipc import Process
 from time import sleep
+from collections import deque
 
 class JDEnv(Env):
     def __init__(self, jd_path, graphics=False, continuous=True):
@@ -22,6 +23,7 @@ class JDEnv(Env):
         self.NODES = np.load(pkg_resources.resource_filename("extra", "nodes.npy"))
 
         self.steps, self.current_node, self.best_node = 0, 1, 1
+        self.velocities = deque(maxlen=200)
 
         self.action_space = Dict({
             "steering": Box(low=-1, high=1, shape=ONE_SHAPE),
@@ -50,6 +52,12 @@ class JDEnv(Env):
         inject(self.process.pid, dll_path.encode("ascii"))
         sleep(0.1)
         self.reset()
+
+    def episode_finished(self):
+        done = self.steps > self.MAX_EPISODE_STEPS + self.current_node * 100
+        if self.steps >= self.MAX_EPISODE_STEPS: self.steps = 0
+
+        return done
 
     def reset(self):
         if self.current_node > self.best_node: self.best_node = self.current_node
@@ -84,6 +92,8 @@ class JDEnv(Env):
         car_node_direction = self.NODES[self.current_node] - self.current_position
         car_node_magnitude, velocity_magnitude = np.linalg.norm(car_node_direction), np.linalg.norm(observation["velocity"])
 
+        self.velocities.append(observation["velocity"])
+
         # Get difference in car direction/node
         if car_node_magnitude != 0 and velocity_magnitude != 0:
             observation["difference"] = np.dot(car_node_direction / car_node_magnitude, observation["velocity"] / velocity_magnitude)
@@ -97,38 +107,35 @@ class JDEnv(Env):
         observation = self.get_observation()
 
         node_distance = euclidean(self.NODES[self.current_node], self.current_position)
-        
+        velocity_mean = np.mean(np.array(self.velocities))
 
         distance_rating = np.reciprocal(node_distance) if node_distance <= 10 else 0
         direction_rating = observation["difference"] if observation["difference"] != 0 else 1
         throttle_rating = (action["throttle"]) ** 2 if action["throttle"] > 0 else 0
+        velocity_rating = velocity_mean if velocity_mean > 0 else 0
 
-        reward = (direction_rating * self.current_node + distance_rating) - sum(observation["wheels"]) * 0.2
+        reward = direction_rating * self.current_node + distance_rating + velocity_rating - sum(observation["wheels"]) * 0.2
 
         # Heavily reward going to node
         if node_distance <= self.PROXIMITY_RADIUS:
             print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
             self.current_node += 1
             self.steps = 0
+            self.velocities.clear()
 
-            reward = self.current_node ^ 2
-
-        # Handle episode resets
-        done = self.current_node == self.NODES.size - 1 or self.steps >= self.MAX_EPISODE_STEPS
-        # done = self.current_node > self.best_node or self.steps >= self.MAX_EPISODE_STEPS
-        if self.steps >= self.MAX_EPISODE_STEPS: self.steps = 0
+            reward = (self.current_node * velocity_rating) ** 2 if velocity_rating > 1 else self.current_node ^ 2
 
         if self.steps % 100 == 0:
             print(f"STEP {self.steps}")
             print("distance rating", node_distance)
             print("direction rating", direction_rating)
             print("throttle_rating", throttle_rating)
+            print("velocity rating", velocity_rating)
             print("reward", reward)
-            print(observation["wheels"])
             print()
 
         info = {} # extra info for debugging
-        return observation, reward, done, info
+        return observation, reward, self.episode_finished(), info
 
     # Display a single game on screen
     # TODO
